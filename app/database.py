94d4194a62d6a6d04f30b23853d09af397519b6c
Artur <wvw999secret@gmail.com>
2026-09-12 17:15:46 +0300

    add TiDB SSL support

--- app/database.py
+++ app/database.py
@@ -1,10 +1,17 @@
+import certifi
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from app.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+#TiDB Cloud требует TLS-соединение, обычный MySQL локально в докере - нет
+#certifi.where() дает путь к пачке доверенных корневых сертификатов,
+#этого достаточно чтобы pymysql поверил сертификату TiDB (он от общеизвестного CA)
+connect_args = {}
+if "tidbcloud.com" in settings.DATABASE_URL:
+    connect_args = {"ssl": {"ca": certifi.where()}}
+
+engine = create_engine(settings.DATABASE_URL, connect_args=connect_args, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
